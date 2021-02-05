d3eb923a9659f9c5c25eb10485a2da90997e5dea
Julian Guillermo Zapata Rugeles (Windows) <juliang.zapata@udea.edu.co>
2021-02-05 11:53:45 -0500

    agregé implementación a la clase espia cuyo objetivo será tener dos punteros a objetos de tipo cañon (tambien podria usarse un tamplate) para pasarse mensajes entre ellas , será la encargada de comunicar a cada objeto por separado sobre la existencia o no de información enemiga

--- solucionParcial/espia.cpp
+++ solucionParcial/espia.cpp
@@ -1,6 +1,13 @@
 #include "espia.h"
 
-Espia::Espia()
+Espia::Espia(CanonOfensivo *canon_ofensivo, CanonDefensivo *canon_defensivo)
+{
+    ptr_canon_defensivo = canon_defensivo;
+    ptr_canon_ofensivo  = canon_ofensivo;
+    controladorEventos = new QTimer();
+}
+
+void Espia::actions()
 {
 
 }
--- solucionParcial/espia.h
+++ solucionParcial/espia.h
@@ -2,12 +2,21 @@
 #define ESPIA_H
 
 #include <QObject>
+#include <QTimer>
+#include <canondefensivo.h>
+#include <canonofensivo.h>
+
 
 class Espia
 {
     Q_OBJECT
 public:
-    Espia();
+    Espia(CanonOfensivo *canon_ofensivo  ,CanonDefensivo *canon_defensivo);
+private:
+    CanonOfensivo *ptr_canon_ofensivo;
+    CanonDefensivo *ptr_canon_defensivo;
+
+private slots:
 };
 
 #endif // ESPIA_H
