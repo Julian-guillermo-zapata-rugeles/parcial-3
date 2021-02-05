a51969bcb385269f33cd199ad4793d34a3813c65
Julian Guillermo Zapata Rugeles (Windows) <juliang.zapata@udea.edu.co>
2021-02-05 08:21:41 -0500

    agregé la función moverParabolicamente y otros atributos útiles de control

--- solucionParcial/movimientos.h
+++ solucionParcial/movimientos.h
@@ -7,9 +7,17 @@ class Movimientos
 public:
     Movimientos();
 protected:
-    float velocidad_x , velocidad_y , tiempo_transcurrido ;
-    short int velocidad_instantanea , angulo ;
+
+    // --------------------- Atributos de la clase Heredables --------------------------------//
+    bool proyectil_en_movimiento;
+    float velocidad_x , velocidad_y , tiempo_transcurrido , posicion_y ;
+    short int velocidad_instantanea , angulo, limite_inferior;
+
+
+    // ---------------------  Métodos de la clase Heredables  -------------------------------//
+
     void configurarProyectil();
+    void moverParabolicamente();
 };
 
 #endif // MOVIMIENTOS_H
