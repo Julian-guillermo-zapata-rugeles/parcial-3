081d8b738875780d20c844f23486666b30369807
Julian Guillermo Zapata Rugeles (Windows) <juliang.zapata@udea.edu.co>
2021-02-05 17:13:05 -0500

    se está implementando la función calcularTrayectorias que permitirá generar trayectorias de disparo de forma automatica

--- solucionParcial/canonofensivo.cpp
+++ solucionParcial/canonofensivo.cpp
@@ -30,15 +30,29 @@ void CanonOfensivo::setAnguloDisparo(signed short value)
 
 
 
-void CanonOfensivo::calcularTrayectorias()
+void CanonOfensivo::calcularTrayectorias(qreal enemy_pos_x, qreal enemy_pos_y)
 {
+    //this->posicion_x = pos().x();
+    //this->posicion_y = pos().x();
+    for(unsigned short int gradosInclinacion = 1 ; gradosInclinacion < 180 ; gradosInclinacion++){
+        // mager para grados de 1 a 180
+        for(unsigned short int velocidadProyectil=1; velocidadProyectil<100;velocidadProyectil++){
+            // margen para velocidad 1 a 100
+            bool exahustedSearch = false;
+            configurarProyectil(velocidad_instantanea,gradosInclinacion,pos().x(),pos().y());
+            while (exahustedSearch==false) {
+                // se trazará una trayectoria ficticia que permitirá obtener coordenadas de avace
 
+            }
+        }
+    }
 }
 
 
 
 
-CanonOfensivo::CanonOfensivo(short posx, short posy):Canon(posx,posy)
+CanonOfensivo::CanonOfensivo(short posx, short posy)
+    :Canon(posx,posy),Movimientos(500,0)
 {
     proyectil_fue_disparado=false;
     anguloDisparo=80;
--- solucionParcial/canonofensivo.h
+++ solucionParcial/canonofensivo.h
@@ -1,8 +1,9 @@
 #ifndef CANONOFENSIVO_H
 #define CANONOFENSIVO_H
 #include "canon.h"
+#include "movimientos.h"
 
-class CanonOfensivo : public Canon
+class CanonOfensivo : public Canon , public Movimientos
 {
 public:
 
@@ -14,11 +15,14 @@ public:
     void setProyectil_fue_disparado(bool value);
     void disparoLibre(); // método que será redefinido (se hereda igualmente desde canon)
     signed short getAnguloDisparo() const;
+
     void setAnguloDisparo(signed short value);
 
 private:
     bool proyectil_fue_disparado;
     signed short int anguloDisparo;
+    void calcularTrayectorias(qreal enemy_pos_x, qreal enemy_pos_y);
+
 };
 
 #endif // CANONOFENSIVO_H
